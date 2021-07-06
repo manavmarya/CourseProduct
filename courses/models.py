b970f021eb0618a2a381c1ccdc11025336621751
manum1 <manu.marya@gmail.com>
2021-07-06 16:00:44 +0530

    Third

--- courses/models.py
+++ courses/models.py
@@ -7,7 +7,7 @@ class Course(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     creator = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'is_staff': True}, related_name="creator")
-    users_enrolled = models.ManyToManyField(AUTH_USER_MODEL, limit_choices_to={'is_staff': False}, related_name="students", null=True)
+    users_enrolled = models.ManyToManyField(AUTH_USER_MODEL, limit_choices_to={'is_staff': False}, related_name="students", blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
