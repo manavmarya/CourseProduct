aff6bf473dff668a8e085d642926c80a92a95d4c
manum1 <manu.marya@gmail.com>
2021-07-07 22:41:58 +0530

    make user add automatically

--- courses/viewsets.py
+++ courses/viewsets.py
@@ -13,11 +13,10 @@ class wishListViewSet(viewsets.ModelViewSet):
     queryset = WishList.objects.all().order_by('-date_added')
     serializer_class = wishListSerializer
 
-    def get_serializer_context(self):
+    '''def get_serializer_context(self):
         context = super(wishListViewSet, self).get_serializer_context()
         context.update({"request": self.request})
-        return context
+        return context'''
 
     def perform_create(self, serializer):
-        request = self.context['request']
-        serializer.save(creator = request.user)
+        serializer.save(creator = self.request.user)
