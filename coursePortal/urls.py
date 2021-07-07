164c83a3af979e6744b071328f8ef099c30c90f4
manum1 <manu.marya@gmail.com>
2021-07-07 17:21:47 +0530

    Add password reset

--- coursePortal/urls.py
+++ coursePortal/urls.py
@@ -26,10 +26,11 @@ urlpatterns = [
     path('', include('users.urls', namespace='users')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True)),
     path('logout/', auth_views.LogoutView.as_view(template_name='login.html', next_page='/login/')),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='password_reset')),
+    url(r'^password_reset/done/$', auth_views.PasswordChangeDoneView.as_view(template_name="password_reset_done.html",email_template_name='password_reset_email.html')),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
+        auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html")),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html")),
     path('api/', include(router.urls))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
